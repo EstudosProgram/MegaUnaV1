0f844834d0985aae93158fa3901d84234f07d214
Milena <milena.cod@gmail.com>
2026-02-10 15:43:00 -0300

    aplicamos o básico de while e variáveis

---
+++ src/br/com/fiap/MegaUnaV1.java
@@ -0,0 +1,33 @@
+package br.com.fiap;
+
+import java.util.Scanner;
+
+public class MegaUnaV1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        byte numEscolhido;
+        byte numSorteado = 45;
+        int tentativas = 5;
+        String resposta = "sim";
+
+        System.out.println("Bem-vindo(a) ao Mega Una!");
+
+        while (resposta.equalsIgnoreCase("sim")) {
+            System.out.println("Digite um número entre 1 e 100:");
+            numEscolhido = sc.nextByte();
+
+            if (numEscolhido == numSorteado) {
+                System.out.println("Ganhou, parabéns!");
+            } else {
+                System.out.println("Que pena! Tente de novo");
+            }
+
+            System.out.println("Gostaria de tentar novamente? (sim/não)");
+            resposta = sc.next();
+        }
+
+        System.out.println("Volte sempre!");
+    }
+}
